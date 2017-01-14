25d1614a65d72917a589b332565460ca234a4943
gilney nathanael mathias <gilney_salvo@hotmail.com>
2017-01-14 15:12:38 -0200

    v0.0.1
    Adicionadas todos os arquivos necessários para o projeto
    Adicionados os métodos da classe Crawler

---
+++ questionnaires_crawler/src/main/java/br/ufsc/tcc/crawler/checker/RulesChecker.java
@@ -0,0 +1,12 @@
+package br.ufsc.tcc.crawler.checker;
+
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
+
+public class RulesChecker {
+
+	public boolean shouldSave(HtmlParseData htmlParseData) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ questionnaires_crawler/src/main/java/br/ufsc/tcc/crawler/main/Main.java
@@ -0,0 +1,8 @@
+package br.ufsc.tcc.crawler.main;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+	}
+}
