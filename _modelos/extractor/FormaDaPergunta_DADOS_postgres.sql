28d795f807e0083a2377018661e7f531fd66c693
gilney nathanael mathias <gilney_salvo@hotmail.com>
2017-01-20 17:17:19 -0200

    v0.2.3
    Reorganizado as formas da pergunta e adicionado algumas a mais

--- _modelos/extractor/FormaDaPergunta_DADOS_postgres.sql
+++ _modelos/extractor/FormaDaPergunta_DADOS_postgres.sql
@@ -1,22 +1,45 @@
 INSERT INTO FormaDaPergunta(descricao) VALUES ('TEXT_INPUT');
+INSERT INTO formadapergunta(descricao) VALUES ('TEXT_INPUT_GROUP');
+INSERT INTO FormaDaPergunta(descricao) VALUES ('TEXT_INPUT_MATRIX');
+
 INSERT INTO FormaDaPergunta(descricao) VALUES ('NUMBER_INPUT');
+INSERT INTO FormaDaPergunta(descricao) VALUES ('NUMBER_INPUT_GROUP');
+
 INSERT INTO FormaDaPergunta(descricao) VALUES ('DATE_INPUT');
+INSERT INTO FormaDaPergunta(descricao) VALUES ('DATE_INPUT_GROUP');
+
 INSERT INTO FormaDaPergunta(descricao) VALUES ('EMAIL_INPUT');
+INSERT INTO FormaDaPergunta(descricao) VALUES ('EMAIL_INPUT_GROUP');
+
 INSERT INTO FormaDaPergunta(descricao) VALUES ('TEL_INPUT');
+INSERT INTO FormaDaPergunta(descricao) VALUES ('TEL_INPUT_GROUP');
+
 INSERT INTO FormaDaPergunta(descricao) VALUES ('TIME_INPUT');
+INSERT INTO FormaDaPergunta(descricao) VALUES ('TIME_INPUT_GROUP');
+
 INSERT INTO FormaDaPergunta(descricao) VALUES ('URL_INPUT');
+INSERT INTO FormaDaPergunta(descricao) VALUES ('URL_INPUT_GROUP');
+
 INSERT INTO FormaDaPergunta(descricao) VALUES ('CHECKBOX_INPUT');
+INSERT INTO FormaDaPergunta(descricao) VALUES ('CHECKBOX_INPUT_GROUP');
+INSERT INTO formadapergunta(descricao) VALUES ('CHECKBOX_INPUT_MATRIX');
+
 INSERT INTO FormaDaPergunta(descricao) VALUES ('RADIO_INPUT');
+INSERT INTO FormaDaPergunta(descricao) VALUES ('RADIO_INPUT_GROUP');
+INSERT INTO FormaDaPergunta(descricao) VALUES ('RADIO_INPUT_MATRIX');
+
 INSERT INTO FormaDaPergunta(descricao) VALUES ('RANGE_INPUT');
 INSERT INTO FormaDaPergunta(descricao) VALUES ('RANGE_INPUT_GROUP');
-INSERT INTO FormaDaPergunta(descricao) VALUES ('TEXT_INPUT_MATRIX');
-INSERT INTO FormaDaPergunta(descricao) VALUES ('RADIO_INPUT_MATRIX');
+
 INSERT INTO FormaDaPergunta(descricao) VALUES ('TEXTAREA');
+INSERT INTO FormaDaPergunta(descricao) VALUES ('TEXTAREA_GROUP');
+INSERT INTO formadapergunta(descricao) VALUES ('TEXTAREA_MATRIX');
+
 INSERT INTO FormaDaPergunta(descricao) VALUES ('SELECT');
-INSERT INTO FormaDaPergunta(descricao) VALUES ('RATING');
-INSERT INTO formadapergunta(descricao) VALUES ('UNKNOWN');
-INSERT INTO formadapergunta(descricao) VALUES ('TEXT_INPUT_GROUP');
+INSERT INTO FormaDaPergunta(descricao) VALUES ('SELECT_GROUP');
+
 INSERT INTO formadapergunta(descricao) VALUES ('MIX_COMP_GROUP');
 INSERT INTO formadapergunta(descricao) VALUES ('MIX_COMP_MATRIX');
-INSERT INTO formadapergunta(descricao) VALUES ('CHECKBOX_INPUT_MATRIX');
-INSERT INTO formadapergunta(descricao) VALUES ('TEXTAREA_MATRIX');
+
+INSERT INTO FormaDaPergunta(descricao) VALUES ('RATING');
+INSERT INTO formadapergunta(descricao) VALUES ('UNKNOWN');
