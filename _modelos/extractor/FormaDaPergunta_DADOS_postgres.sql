fd2f7854aa3f16e757efc6d4c8927759e4023d6f
gilney nathanael mathias <gilney_salvo@hotmail.com>
2017-04-21 13:41:07 -0300

    v0.2.6
    Adição de algumas novas formas de pergunta

--- _modelos/extractor/FormaDaPergunta_DADOS_postgres.sql
+++ _modelos/extractor/FormaDaPergunta_DADOS_postgres.sql
@@ -4,21 +4,27 @@ INSERT INTO FormaDaPergunta(descricao) VALUES ('TEXT_INPUT_MATRIX');
 
 INSERT INTO FormaDaPergunta(descricao) VALUES ('NUMBER_INPUT');
 INSERT INTO FormaDaPergunta(descricao) VALUES ('NUMBER_INPUT_GROUP');
+INSERT INTO FormaDaPergunta(descricao) VALUES ('NUMBER_INPUT_MATRIX');
 
 INSERT INTO FormaDaPergunta(descricao) VALUES ('DATE_INPUT');
 INSERT INTO FormaDaPergunta(descricao) VALUES ('DATE_INPUT_GROUP');
+INSERT INTO FormaDaPergunta(descricao) VALUES ('DATE_INPUT_MATRIX');
 
 INSERT INTO FormaDaPergunta(descricao) VALUES ('EMAIL_INPUT');
 INSERT INTO FormaDaPergunta(descricao) VALUES ('EMAIL_INPUT_GROUP');
+INSERT INTO FormaDaPergunta(descricao) VALUES ('EMAIL_INPUT_MATRIX');
 
 INSERT INTO FormaDaPergunta(descricao) VALUES ('TEL_INPUT');
 INSERT INTO FormaDaPergunta(descricao) VALUES ('TEL_INPUT_GROUP');
+INSERT INTO FormaDaPergunta(descricao) VALUES ('TEL_INPUT_MATRIX');
 
 INSERT INTO FormaDaPergunta(descricao) VALUES ('TIME_INPUT');
 INSERT INTO FormaDaPergunta(descricao) VALUES ('TIME_INPUT_GROUP');
+INSERT INTO FormaDaPergunta(descricao) VALUES ('TIME_INPUT_MATRIX');
 
 INSERT INTO FormaDaPergunta(descricao) VALUES ('URL_INPUT');
 INSERT INTO FormaDaPergunta(descricao) VALUES ('URL_INPUT_GROUP');
+INSERT INTO FormaDaPergunta(descricao) VALUES ('URL_INPUT_MATRIX');
 
 INSERT INTO FormaDaPergunta(descricao) VALUES ('CHECKBOX_INPUT');
 INSERT INTO FormaDaPergunta(descricao) VALUES ('CHECKBOX_INPUT_GROUP');
@@ -42,5 +48,9 @@ INSERT INTO formadapergunta(descricao) VALUES ('MIX_COMP_GROUP');
 INSERT INTO formadapergunta(descricao) VALUES ('MIX_COMP_MATRIX');
 
 INSERT INTO FormaDaPergunta(descricao) VALUES ('RATING');
-INSERT INTO formadapergunta(descricao) VALUES ('UNKNOWN');
+INSERT INTO FormaDaPergunta(descricao) VALUES ('RATING_GROUP');
+
 INSERT INTO FormaDaPergunta(descricao) VALUES ('MULTI_COMP');
+INSERT INTO FormaDaPergunta(descricao) VALUES ('MULTI_COMP_GROUP');
+
+INSERT INTO formadapergunta(descricao) VALUES ('UNKNOWN');
