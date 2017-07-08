5ae7b16549f9620763baa15ff0c9b1bb7c4759a6
gilney nathanael mathias <gilney_salvo@hotmail.com>
2017-07-08 15:27:24 -0300

    v0.2.8
    Adição de mais algumas querys envolvendo o IMAGE_RADIO_INPUT

--- _modelos/extractor/FormaDaPergunta_DADOS_postgres.sql
+++ _modelos/extractor/FormaDaPergunta_DADOS_postgres.sql
@@ -32,6 +32,9 @@ INSERT INTO FormaDaPergunta(descricao) VALUES ('CHECKBOX_INPUT_GROUP');
 INSERT INTO formadapergunta(descricao) VALUES ('CHECKBOX_INPUT_MATRIX');
 
 INSERT INTO FormaDaPergunta(descricao) VALUES ('IMAGE_RADIO_INPUT');
+INSERT INTO FormaDaPergunta(descricao) VALUES ('IMAGE_RADIO_INPUT_GROUP');
+INSERT INTO FormaDaPergunta(descricao) VALUES ('IMAGE_RADIO_INPUT_MATRIX');
+
 INSERT INTO FormaDaPergunta(descricao) VALUES ('RADIO_INPUT');
 INSERT INTO FormaDaPergunta(descricao) VALUES ('RADIO_INPUT_GROUP');
 INSERT INTO FormaDaPergunta(descricao) VALUES ('RADIO_INPUT_MATRIX');
